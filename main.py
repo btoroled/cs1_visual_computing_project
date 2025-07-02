532014d19ca02cb76c00a20b5009b7b676c27694
Benjamin Toro <73610569+btoroled@users.noreply.github.com>
2025-07-01 20:44:27 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# Datos de entrenamiento
+X = np.array([1, 2, 3, 4, 5])  # Entradas
+Y = 0.5 * X  # Salidas reales
+
+# Inicialización del peso
+w = np.random.rand()
+
+# Parámetros de entrenamiento
+epochs = 10000
+learning_rate = 0.0001
+
+# Entrenamiento
+for epoch in range(epochs):
+    for x, y_real in zip(X, Y):
+        y_pred = w * x
+        error = y_pred - y_real
+        loss = 0.5 * (error ** 2) # no se necesita en el código
+
+        # Gradiente
+        gradient = error * x
+
+        # Actualización del peso
+        w -= learning_rate * gradient
+
+    if epoch % 100 == 0:
+        print(f'Epoch {epoch}, Weight {w:.4f}')
+
+# Resultado
+print(f'Final weight: {w:.4f}')
